64e27c3f5d47fdc9fd7a199f16aac6dedd5bb134
Jason Tilley <jttilley007@yahoo.com>
2020-09-07 21:00:13 -0600

    moved routes back to their own js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,25 @@ const notes = require("../data/notes");
 const fs = require("fs");
 const path = require("path");
 
+const OUTPUT_DIR = path.resolve(__dirname, "../db");
+const outputPath = path.join(OUTPUT_DIR, "db.json");
+// console.log('OUTPUT_DIR: ', OUTPUT_DIR);
+// console.log('outputPath: ', outputPath);
+
+//set next id
+let nextId = 1;
+if (notes.length > 0) {
+    nextId += notes[notes.length-1].id
+}
+
+// console.log('nextId: ', nextId);
+
+const saveNotes = () => {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, JSON.stringify(notes, null, 2), "utf-8");
+};
 
 
 module.exports = function(app) {
@@ -9,31 +28,35 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(notes);
     });
-
+    
     app.post("/api/notes", function(req, res) {
-        console.log(req.body);
+        // console.log(req.body);
         const b = req.body;
     
         const newNote = {
             title: b.title,
             text: b.text,
-            id: id
+            id: nextId
         }
     
-        id ++; // increment id
+        nextId ++; // increment id
     
         notes.push(newNote);
         console.log('notes: ', notes);
     
         saveNotes();
-        return res(true);
+        res.json({ok : true});
     });
-
+    
     app.delete("/api/notes/:id", function(req, res) {
+        // console.log('req.params: ', req.params);
         const id = req.params.id;
-        const index = notes.findIndex(cur => cur.id === id);
-        notes.splice(index,1);
+        // console.log('id: ', id);
+        const index = notes.findIndex(cur => cur.id === parseInt(id));
+        // console.log('index: ', index);
+        notes.splice(index,1); //remove index from notes
         saveNotes();
+        res.json({ok: true});
     });
 }
 
--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const notes = require("./data/notes");
 const fs = require("fs");
 const path = require("path");
 
-//set next id
-let nextId = 1;
-if (notes.length > 0) {
-    nextId += notes[notes.length-1].id
-}
-
-console.log('nextId: ', nextId);
-
 // create an express server
 const app = express();
 
@@ -24,62 +16,11 @@ app.use(express.static('public')); //Serves resources from public folder
 // set port
 var PORT = process.env.PORT || 8080;
 
-const OUTPUT_DIR = path.resolve(__dirname, "db");
-const outputPath = path.join(OUTPUT_DIR, "db.json");
-console.log('OUTPUT_DIR: ', OUTPUT_DIR);
-console.log('outputPath: ', outputPath);
 
-const saveNotes = () => {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR);
-    }
-    fs.writeFileSync(outputPath, JSON.stringify(notes, null, 2), "utf-8");
-};
 
 // routes -------------------------
-// require("./routes/apiRoutes")(app);
-app.get("/api/notes", function(req, res) {
-    res.json(notes);
-});
-
-app.post("/api/notes", function(req, res) {
-    console.log(req.body);
-    const b = req.body;
-
-    const newNote = {
-        title: b.title,
-        text: b.text,
-        id: nextId
-    }
-
-    nextId ++; // increment id
-
-    notes.push(newNote);
-    console.log('notes: ', notes);
-
-    saveNotes();
-    res.json({ok : true});
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    console.log('req.params: ', req.params);
-    const id = req.params.id;
-    console.log('id: ', id);
-    const index = notes.findIndex(cur => cur.id == id);
-    console.log('index: ', index);
-    notes.splice(index,1);
-    saveNotes();
-    res.json({ok: true});
-});
-
-// require("./routes/htmlRoutes")(app);
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
-});
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // listener
 app.listen(PORT, () => {
